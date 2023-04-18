e932fbc4c3ded3486357caf5f502a1fbcf40c78f
Oleh Kliapko <goldclub1977@gmail.com>
2023-04-18 15:55:36 +0300

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://goldclub1977:Ro300979koa@kliapko-restapi.ob2szcn.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const app = require("./app");
+const { error } = require("./schemas/contacts");
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });

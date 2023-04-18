b07c6b2feea011c45691920dc93903a288032c09
Oleh Kliapko <goldclub1977@gmail.com>
2023-04-18 17:55:00 +0300

    add .env

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://goldclub1977:Ro300979koa@kliapko-restapi.ob2szcn.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-module.exports = {
-  DB_HOST,
-};
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running on port: 3000");
     })
   )
